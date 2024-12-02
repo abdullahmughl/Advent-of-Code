7d47c5c57dd28448112c3ff43caf5d7d94fde931
Abdullah Mughal <abdullah.mughal@convertdigital.com.au>
2024-12-02 11:13:21 +0500

    Day 2: Part 2

--- 2024/Day 2/index.js
+++ 2024/Day 2/index.js
@@ -38,3 +38,48 @@ lines.map((line) => {
 // ---------------------------------------------------- //
 
 // Part 2
+
+let numberOfDampenerSafeReports = 0
+
+const checkSafeValues = (line) => {
+  let isLineSafe = true
+  for(let i = 1; i<line.length; i++) {
+    const levelDistance = line[i - 1] - line[i]
+    const diff = Math.abs(levelDistance)
+
+    if (diff > 3 || diff < 1){
+      isLineSafe = false
+    }
+
+    if (line[0] > line[1] && diff != levelDistance || line[0] < line[1] && diff != -levelDistance) {
+      isLineSafe = false
+    }
+  }
+
+  return isLineSafe
+}
+
+lines.map((line) => {
+  let isSafe = checkSafeValues(line)
+
+  if (isSafe) {
+    numberOfDampenerSafeReports++
+  } else {
+    let isNewLineSafe = false
+    for (let i = 0; i < line.length; i++) {
+      const n = [...line]
+      n.splice(i, 1)
+
+      const result = checkSafeValues(n)
+
+      if (result) {
+        isNewLineSafe = true
+      }
+    }
+    if (isNewLineSafe) {
+      numberOfDampenerSafeReports++
+    }
+  }
+})
+
+// console.log(numberOfDampenerSafeReports)
