6e357057eeb8ac2e485688f46dd2fcd2ffb93950
Abdullah Mughal <abdullah.mughal@convertdigital.com.au>
2024-12-03 12:27:29 +0500

    Day 3: Part 2

--- 2024/Day 3/index.js
+++ 2024/Day 3/index.js
@@ -13,9 +13,25 @@ x.map((val) => {
   result = result + (val[1] * val[2])
 })
 
-console.log(result)
+// console.log(result)
 
 // ---------------------------------------------------- //
 
 // Part 2
 
+let sum = 0
+let isValid = true
+
+const z = [...(inputFile).matchAll(/(?:mul\((\d{1,3}),(\d{1,3})\))+?|(do\(\))|(don't\(\))/g)];
+
+z.map((val) => {
+  if (val[0] == "do()") {
+      isValid = true;
+  } else if (val[0] == "don't()") {
+      isValid = false;
+  } else if (isValid) {
+      sum = sum + (val[1] * val[2]);
+  }
+})
+
+console.log(sum);
