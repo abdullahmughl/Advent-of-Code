84e95d3925fb2b63415115a8c36576550eab3c99
Abdullah Mughal <abdullah.mughal@convertdigital.com.au>
2024-12-05 11:45:26 +0500

    Day 5: Part 2

--- 2024/Day 5/index.js
+++ 2024/Day 5/index.js
@@ -44,4 +44,42 @@ validLines.map((line) => {
   const values = line.split(',')
   middleCount = middleCount + parseInt(values[Math.floor(values.length / 2)])
 })
-console.log(middleCount)
+// console.log(middleCount)
+
+// ---------------------------------------------------- //
+
+// Part 2
+let invalidLines = []
+let orderedMiddleCount = 0
+
+lines.map((line) => {
+  const values = line.split(',')
+  let isLineValid = true
+  for (let i = 0; i< values.length - 1 ; i++) {
+    if (![...values].splice(i+1, values.length - 1).every((num) => rulesSet[values[i]] && rulesSet[values[i]].includes(num))){
+      isLineValid = false
+      break;
+    }
+  }
+
+
+  if (!isLineValid) {
+    const line = values.sort((a,b) =>{
+      if (rulesSet[b] && rulesSet[b].includes(a)) {
+        return 0
+      } else if (rulesSet[a] && rulesSet[a].includes(b)){
+        return -1
+      } else {
+        return 1
+      }
+    }
+    )
+    invalidLines.push(line)
+  }
+})
+
+
+invalidLines.map((line) => {
+  orderedMiddleCount = orderedMiddleCount + parseInt(line[Math.floor(line.length / 2)])
+})
+console.log(orderedMiddleCount)
